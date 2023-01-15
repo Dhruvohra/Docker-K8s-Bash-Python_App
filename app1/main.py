cd470c735f2ee4d38906134b3450d23e2acee950
Dhruv Vohra <dhruvohra@gmail.com>
2023-01-15 15:54:36 +0530

    Exception Handling added for json

--- app1/main.py
+++ app1/main.py
@@ -6,18 +6,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	my_json = {
-		"John": "Pepperoni",
-		"Mary": "Cheese",
-		"Tim": "Mushroom"
-	}
-	return my_json
+    my_json = {
+	"John": "Pepperoni",
+	"Mary": "Cheese",
+	"Tim": "Mushroom"
+    }
+    return my_json
 
 @app.route('/file')
 def open_file():
-     with open('fcc.json', 'r') as fcc_file:
-         fcc_data = json.load(fcc_file)
-         return fcc_data
+    with open('fcc.json', 'r') as fcc_file:
+        try:
+            fcc_data = json.load(fcc_file)
+        except ValueError as err:
+            return "Not a Valid JSON FORMAT"
+        return fcc_data
 
 if __name__ == "__main__":
     app.run(debug=True)
